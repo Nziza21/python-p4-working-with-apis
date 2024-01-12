965b243bb7f3eb5c66010f6815b90cc82e1919a0
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2024-01-12 15:47:56 +0300

    completed

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,29 @@
-
 import requests
 import json
 
 class GetPrograms:
+    def __init__(self, api_key):
+        self.api_key = api_key
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        URL = "https://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        headers = {'Authorization': f'Bearer {self.api_key}'}
+        response = requests.get(URL, headers=headers)
+        return response.content
 
-    response = requests.get(URL)
-    return response.content
+    def program_school(self):
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            if "agency" in program:
+                programs_list.append(program["agency"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+        return programs_list
+
+if __name__ == "__main__":
+    api_key = "https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1"
+    programs = GetPrograms(api_key)
+    programs_schools = programs.program_school()
+
+    for school in set(programs_schools):
+        print(school)
